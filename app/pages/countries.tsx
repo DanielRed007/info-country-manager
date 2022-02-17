9d6cfc54fc2e021a04d3b30fdcf0dece58cae71d
DanielRed007 <danielrojo1927@gmail.com>
2022-02-17 08:52:32 -0300

    current advance

---
+++ app/pages/countries.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Countries: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      Hello Country List
+    </div>
+  )
+}
+
+export default Countries;
