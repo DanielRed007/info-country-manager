4df7796b87ab6de452eb799d7d1f08c3d051da1d
DanielRed007 <danielrojo1927@gmail.com>
2022-03-09 23:02:03 -0300

    first table implementation

--- app/pages/countries.tsx
+++ app/pages/countries.tsx
@@ -1,6 +1,13 @@
 import { useEffect } from "react";
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import { getServerData } from './client/util/apiUtil';
 
 export default function Detail ({ listData }){
@@ -11,8 +18,40 @@ export default function Detail ({ listData }){
     return (
         <>
             <Container maxWidth="lg">
-                <Box sx={{ flexGrow: 1 }}>
-                    <h1>Countries</h1>
+                <Box sx={{ flexGrow: 1, marginTop: "3rem" }}>
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="left">Name</TableCell>
+                                    <TableCell align="left">Native</TableCell>
+                                    <TableCell align="left">Flag</TableCell>
+                                    <TableCell align="left">Capital</TableCell>
+                                    <TableCell align="left">Continent</TableCell>
+                                    <TableCell align="left">Currency</TableCell>
+                                    <TableCell align="left">Phone</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {listData.countries.map((row) => (
+                                    <TableRow
+                                    key={row.name}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell align="left" component="th" scope="row">
+                                            {row.name}
+                                        </TableCell>
+                                        <TableCell align="left">{row.native}</TableCell>
+                                        <TableCell align="left">{row.emoji}</TableCell>
+                                        <TableCell align="left">{row.capital}</TableCell>
+                                        <TableCell align="left">{row.continent}</TableCell>
+                                        <TableCell align="left">{row.currency}</TableCell>
+                                        <TableCell align="left">+ {row.phone}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </Box>
             </Container>
         </>
